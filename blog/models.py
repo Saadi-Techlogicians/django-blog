08816e0bf6f6f7e45a6acd179096646ad9d1044d
Logician Saadi <17-33805-1@student.aiub.edu>
2022-01-04 21:26:01 +0600

    register form is added in users app

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,7 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    date_posted = models.DateTimeField()
+    date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title 
--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-Post = [
-    {
-        'author':'Saadi mohammed',
-        'title':'Blog Post 1',
-        'content':' First post content',
-        'date_posted': 'January 3, 2021'
-    },
-    {
-        'author':'Neaz mohammed',
-        'title':'Blog Post 2',
-        'content':' Second post content',
-        'date_posted': 'January 4, 2021'
-    }
-]
+from blog.models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': Post
+        'posts': Post.objects.all()
     }
     return render(request, "blog/home.html", context)
 
 def about(request):
     title = 'About'
-    return render(request, "blog/about.html", {'title':title})
+    return render(request, "blog/about.html", {'title': title})
